facfed8807e9f930ece45daf6d95063757f0a11f
Jack DesCombes <jpdesc@alumni.stanford.edu>
2023-02-07 19:55:03 -0800

    Add geocoding API logic

--- app.js
+++ app.js
@@ -11,34 +11,60 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apiKey = "1126ea142dfe633b53a669936a917613";
+  const geocodingURL =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    query +
+    "&limit=5&appid=" +
+    apiKey;
+
+  let lattitude = "";
+  let longitude = "";
+
+  https.get(geocodingURL, function (response) {
+    response.on("data", function (data) {
+      const queryData = JSON.parse(data);
+      lattitude = queryData[0].lat;
+      console.log(lattitude);
+      longitude = queryData[0].lon;
+    });
+  });
+  console.log("Lattitude = " + lattitude);
+
+  const units = "imperial";
+  const onecallUrl =
+    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+    lattitude +
+    "&lon=" +
+    longitude +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    units;
+  https.get(onecallUrl, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+      const temp = weatherData.current.temp;
+      const description = weatherData.current.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "<h1>The temperature in" +
+          query +
+          " is " +
+          temp +
+          "degrees Celsius.</h1>"
+      );
+      res.write("<p>The weather is currently " + description + "</p>");
+      res.write("<img src=" + imageUrl + ">");
+      res.send();
+    });
+  });
+  res.send("Server is running");
 });
-//  const query = "London";
-//  const apiKey = "1126ea142dfe633b53a669936a917613";
-//  const units = "imperial";
-//  const url =
-//    "https://api.openweathermap.org/data/3.0/onecall?" +
-//    query +
-//    "&appid=" +
-//    url +
-//    "&units=" +
-//    units;
-//  https.get(url, function (response) {
-//    console.log(response.statusCode);
-//    response.on("data", function (data) {
-//      const weatherData = JSON.parse(data);
-//      const temp = weatherData.current.temp;
-//      const description = weatherData.current.weather[0].description;
-//      const icon = weatherData.weather[0].icon;
-//      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-//      res.write(
-//        "<h1>The temperature in London is " + temp + "degrees Celsius.</h1>"
-//      );
-//      res.write("<p>The weather is currently " + weatherDescription + "</p>");
-//      res.write("<img src=" + imageUrl + ">");
-//      res.send();
-//    });
-//  });
-//  res.send("Server is running");
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
